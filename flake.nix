3df4baa342d769fe22900dba761359c4aff3eb75
Hala Elhamahmy <hala.elhamahmy@gmail.com>
2025-06-19 03:29:41 +0300

    first commit

--- flake.nix
+++ flake.nix
@@ -10,18 +10,24 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-      in
-      {
+
+        python-with-packages = pkgs.python311.withPackages (ps: with ps; [
+          pip
+          dagger-io
+        ]);
+      in {
         devShells.default = pkgs.mkShell {
           buildInputs = [
             pkgs.nodejs_22
-            pkgs.python311
-            pkgs.python311Packages.pip
+            python-with-packages
             pkgs.git
           ];
+
           shellHook = ''
-            pip install --user dagger-io
-            npm install
+            echo "Dev environment ready."
+            echo "Python: $(python --version)"
+            echo "Node: $(node --version)"
+            echo "Dagger: $(python -m dagger --version || true)"
           '';
         };
       });
